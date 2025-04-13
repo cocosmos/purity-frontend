99c1c35dbcc7c0c70118391d3e6845a2cc79d3e5
Cocosmos <mipam.guillot@crea-inseec.com>
2025-04-13 19:57:28 +0200

    fix: image size

--- components/Molecules/GameResultsCard.tsx
+++ components/Molecules/GameResultsCard.tsx
@@ -119,6 +119,7 @@ export function GameResultsCard({
                         <Image
                           source={{ uri: category.image }}
                           width={60}
+                          height="100%"
                           borderRadius="$2"
                           alt={category.name}
                         />
